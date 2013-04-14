c7f5705eb1e1edef1fb40dbac0bd9f9f48d82ef1
Iain Beeston <iain.beeston@gmail.com>
2013-04-14 21:54:57 +1000

    Made sure that if you're browsing in incognito mode, then any new windows opened will also be in incognito mode

--- detach_new_tabs.js
+++ detach_new_tabs.js
@@ -2,7 +2,7 @@ function detachTab(tab) {
     // ignore the first tab in each window
     if (tab.index === 0) return;
 
-    chrome.windows.create({'tabId': tab.id});
+    chrome.windows.create({'tabId': tab.id, 'incognito': tab.incognito});
 };
 
 chrome.tabs.onCreated.addListener(detachTab);
