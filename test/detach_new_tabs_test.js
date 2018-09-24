bf8f6262f55032ecdccad5a13b0b46d07db38e01
Iain Beeston <iainbeeston@users.noreply.github.com>
2018-09-24 09:23:27 +0100

    Made eslint enforce semicolons

--- test/detach_new_tabs_test.js
+++ test/detach_new_tabs_test.js
@@ -9,26 +9,26 @@
   afterEach(function() {
     chrome.windows.get.flush();
     chrome.windows.create.flush();
-  })
+  });
 
   describe('when a new tab is created', function() {
     it('moves the tab to a new window', function() {
       chrome.windows.get.yields({});
       chrome.tabs.onCreated.dispatch({id: 12345});
       sinon.assert.calledWithMatch(chrome.windows.create, sinon.match.has('tabId', 12345));
-    })
+    });
 
     it('does nothing if the tab is the first tab in that window', function() {
       chrome.tabs.onCreated.dispatch({index: 0});
-      sinon.assert.notCalled(chrome.windows.get)
-      sinon.assert.notCalled(chrome.windows.create)
-    })
+      sinon.assert.notCalled(chrome.windows.get);
+      sinon.assert.notCalled(chrome.windows.create);
+    });
 
     it('does nothing if the tab is pinned', function() {
       chrome.tabs.onCreated.dispatch({pinned: true});
-      sinon.assert.notCalled(chrome.windows.get)
-      sinon.assert.notCalled(chrome.windows.create)
-    })
+      sinon.assert.notCalled(chrome.windows.get);
+      sinon.assert.notCalled(chrome.windows.create);
+    });
 
     it('reuses the incognito status from the original tab', function() {
       chrome.windows.get.yields({});
@@ -45,7 +45,7 @@
 
   describe('when the extension is installed', function() {
     it('queries all tabs and moves them to a new window', function() {
-      chrome.tabs.query.yields([{ id: 1 }, { id: 2 }])
+      chrome.tabs.query.yields([{ id: 1 }, { id: 2 }]);
       chrome.windows.get.yields({});
       chrome.runtime.onInstalled.dispatch();
       sinon.assert.calledWithMatch(chrome.windows.create, sinon.match.has('tabId', 1));
