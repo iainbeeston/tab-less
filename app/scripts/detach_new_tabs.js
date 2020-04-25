21ce6549ab7868806cccae42863c79463b4a84aa
nate-wilkins <nate-wilkins@code-null.com>
2020-04-25 16:32:29 -0700

    Add support for removing tabbar

--- app/scripts/detach_new_tabs.js
+++ app/scripts/detach_new_tabs.js
@@ -1,21 +1,42 @@
 'use strict';
 
-var detachTab = function(tab) {
-  if (tab.index === 0 || tab.pinned) {
+var NEW_TAB_POPUP = true;
+
+var detachTab = function(tab, tabType) {
+  if (tab.url === 'chrome://newtab/' || tab.pinned) {
     return;
   }
 
   chrome.windows.get(tab.windowId, function(oldWindow) {
-    chrome.windows.create({tabId: tab.id, incognito: tab.incognito, state: oldWindow.state});
+    chrome.windows.create({tabId: tab.id, incognito: tab.incognito, state: oldWindow.state, type: tabType});
   });
 };
 
 var detachAllTabs = function() {
   chrome.tabs.query({}, function(tabs) {
-    tabs.forEach(detachTab);
+    tabs.forEach(function (tab) {
+      var tabType = NEW_TAB_POPUP ? 'popup' : 'normal';
+      detachTab(tab, tabType);
+    });
   });
 };
+
+var toggleActiveTab = function() {
+  chrome.windows.getCurrent(function(win) {
+    var windowType = win.type;
+    chrome.tabs.getAllInWindow(function(tabs) {
+      var tab = tabs.filter(function(tab) { return tab.active; })[0];
+      var tabType = windowType === 'normal' ? 'popup' : 'normal';
+      detachTab(tab, tabType);
+    });
+  });
+}
 
 chrome.runtime.onInstalled.addListener(detachAllTabs);
 chrome.runtime.onStartup.addListener(detachAllTabs);
 chrome.tabs.onCreated.addListener(detachTab);
+chrome.commands.onCommand.addListener(function (command){
+  if (command === 'toggle-popup-mode') {
+    toggleActiveTab();
+  }
+});
